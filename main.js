db12da123895cd4494fd312505a373ed59cccd1c
AbdiDR <abdidwiramdani@gmail.com>
2023-03-09 11:01:36 +0700

    push js day 1

---
+++ main.js
@@ -0,0 +1,21 @@
+const book = { name: "Hujan", price: "10000", author: "Tere Liye" };
+
+function bookpurchase(name, indc, intax) {
+  let result = 0;
+
+  const price = book.price;
+  const dc = price * (indc / 100);
+  const dcprice = price - dc;
+  const tax = dcprice * (intax / 100);
+  const total = dcprice + tax;
+
+  console.group();
+  console.log("Original Price", price);
+  console.log("Discount Amount", dc);
+  console.log("Price after Discount", dcprice);
+  console.log("Tax Amount", tax);
+  console.log("Price after Tax", total);
+  console.groupEnd();
+}
+
+bookpurchase(book, 25, 10);
